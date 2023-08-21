08be956ff9925e298d825131ea3e65f22641c90e
PADVISOHAN <padvisohan444@gmail.com>
2023-08-21 10:37:58 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "mad_practical3_22172022011"
+include(":app")
+ 
